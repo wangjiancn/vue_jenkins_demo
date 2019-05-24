6e3409ed34f38da0f1f39be504f984bc1c7adb9f
wangjiancn <wangjianchn@outlook.com>
2019-05-25 00:00:23 +0800

    feat:首页/文章列表增加侧边栏

--- src/views/post/List.vue
+++ src/views/post/List.vue
@@ -1,35 +1,46 @@
 <template>
-  <div class>
-    <el-row :gutter="12">
-      <el-col
-        v-for="item in items"
-        :key="item.id"
-        class="post-card"
-        :span="8"
+  <div class="post-container">
+    <div
+      v-for="item in items"
+      :key="item.id"
+      class="post"
+    >
+      <div class="post-body">
+        <h1 class="post-title">
+          <router-link :to="`/post/${item.id}`">{{ item.title }}</router-link>
+        </h1>
+        <div class="post-meta">
+          <span class="post-meta">作者:{{ item.author ? item.author.nickname || item.author.username : '' }}</span>
+          <span class="post-meta">最后更新:{{ $dayjs(item.last_modified,{locale:'zh-cn'}).fromNow() }}</span>
+          <span
+            v-if="item.cat"
+            class="post-meta"
+          >分类: <router-link :to="`/post?cat__id=${item.cat.id}`">{{ item.cat.name }}</router-link> </span>
+          <span class="post-meta">标签:
+            <router-link
+              v-for="tag in item.tags"
+              :key="tag.uuid"
+              :to="'/post?tags__id='+tag.id"
+            >
+              {{ tag.name }}</router-link> </span>
+        </div>
+        <p>{{ item.desc }}</p>
+      </div>
+    </div>
+    <div class="pagination">
+      <el-pagination
+        :page-size="20"
+        :pager-count="11"
+        layout="prev, pager, next"
+        :total="1000"
       >
-        <el-card shadow="always">
-          <div>
-            <h1>{{ item.title }}</h1>
-            <span>作者:{{ item.author ? item.author.nickname || item.author.username : '' }}</span>
-          </div>
-          <div style="margin-bottom:10px;float:right">
-            <el-button
-              type="info"
-              size="mini"
-              @click="goToEditor(item.id)"
-            >View</el-button>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
+      </el-pagination>
+    </div>
   </div>
 </template>
-<script>
-import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import { fetchArticles } from '@/api/article'
 
-dayjs.extend(utc)
+<script>
+import { fetchArticles } from '@/api/article'
 
 export default {
   data() {
@@ -37,68 +48,78 @@ export default {
       items: [],
     }
   },
-  async mounted() {
+  watch: {
+    $route() {
+      this.loadItems()
+    },
+  },
+  async created() {
     this.loadItems()
   },
   methods: {
     async loadItems() {
+      console.log('TCL: loadItems -> this.$route.query', this.$route.query)
       const {
         data: { objects, meta },
-      } = await fetchArticles()
+      } = await fetchArticles(this.$route.query)
       console.log('TCL: loadItems -> objects, meta', objects, meta)
       this.items = objects
-    },
-    goToEditor(id) {
-      this.$router.push(`/post/${id}`)
     },
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.post-card {
-  height: 250px;
-  white-space: nowrap;
-  margin-bottom: 10px;
+.post-container {
+  padding-right: 40px;
 }
 .post {
-  min-width: 50%;
-  border: 1px solid gray;
-  border-radius: 5px;
-  float: left;
+  white-space: nowrap;
+  margin-bottom: 0.6rem;
+  border: 1px solid #999999;
+  border-radius: 0.2rem;
+  width: 100%;
+  padding: 10px;
+  .post-body {
+    margin: 5px;
+  }
   .post-title {
+    font-size: 1.8rem;
     margin: 0 auto;
-    margin-left: 30px;
     text-align: left;
   }
-  .time {
-    font-size: 13px;
-    color: #999;
+  .post-meta {
+    font-size: 0.7rem;
+    color: #888888;
+    display: inline;
   }
+  p {
+    margin: 0;
+    font-size: 1rem;
+  }
+  a,
+  a:hover,
+  a:active {
+    text-decoration: none;
+    color: inherit;
+  }
+}
+.pagination {
+  position: absolute;
+  bottom: 10px;
 
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+  // 居中方案
+  // case1
+  width: 100%;
+  text-align: center;
+  margin-bottom: -40px;
 
-  .button {
-    padding: 0;
-    float: right;
-  }
+  //case2
+  // width: 100%;
+  // display: flex;
 
-  .image {
-    width: 100%;
-    display: block;
-  }
-
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: '';
-  }
-
-  .clearfix:after {
-    clear: both;
-  }
+  // case3
+  // left: 48%;
+  // transform: translateX(-50%);
 }
 </style>
