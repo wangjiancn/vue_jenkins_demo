e54b855e38f2925f78bd70ed71b7bf9d313806da
wangjiancn <wangjianchn@outlook.com>
2019-06-20 22:11:14 +0800

    style:修改purgecss白名单

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
   whitelistPatterns: [/^el/, /^markdown/, /.*mavon.*/, /.*ant.*/], //ElementUI加入白名单，否则会把所有的双下划线去掉
-  whitelistPatternsChildren: [/.*ant.*/],
+  whitelistPatternsChildren: [/.*ant.*/, /^el.*/, /^markdown/, /.*mavon.*/],
 })
 
 module.exports = {
